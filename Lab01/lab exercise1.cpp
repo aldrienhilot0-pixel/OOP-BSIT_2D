8d67a48b91b29cdc73c751ce1e310e756ccf0c98
jnfadrilan2023 <junnel.fadrilan@gmail.com>
2026-02-25 08:58:54 +0800

    Added new line of Code to perform Addition

--- Lab01/lab exercise1.cpp
+++ Lab01/lab exercise1.cpp
@@ -4,5 +4,11 @@ main(){
 	
 	cout <<"Welcome to my OOP Projects" <<endl;
 	
+	cout<<"Perform Simple Calculation" <<endl;
+	
+	int total = 10 + 5;
+	
+	cout <<"The total of 5 + 10 is " <<endl;
+	
 	return 0;
 }
